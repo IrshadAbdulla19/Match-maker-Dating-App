e36bff5c528b33eecee76e3f4ef5e5b9bb161749
IrshadAbdulla19 <irshadabdulla19@gmail.com>
2023-10-18 12:18:00 +0530

    add dependancy injction

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_new_project/applications/Auth/authentication_bloc.dart';
+import 'package:my_new_project/domain/core/di/injuctable.dart';
 import 'package:my_new_project/presentetion/pages/splash/splash_screen.dart';
 
 const token = "for show";
-void main() {
+void main() async {
+  await cofigeinjection();
   runApp(const Myapp());
 }
 
@@ -11,9 +15,16 @@ class Myapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(),
-        home: SplashScreen());
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => getIt<AuthenticationBloc>(),
+        ),
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(),
+          home: SplashScreen()),
+    );
   }
 }
